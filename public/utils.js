fdecb9c02216332622fbe4a19837643b90d3f25f
Filip Talaga <talaga.filip@gmail.com>
2023-05-25 18:25:05 +0200

    Expose saturation and light for random color function

--- public/utils.js
+++ public/utils.js
@@ -1,3 +1,4 @@
 export const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-export const getRandomColor = () => `hsl(${getRandomInt(0, 360)}, 80%, 50%)`;
+export const getRandomColor = ({ s = 0.8, l = 0.5 } = {}) =>
+  `hsl(${getRandomInt(0, 360)}, ${s * 100}%, ${l * 100}%)`;
