dc0f5df89b583123a781b626c9238baa8bd8a64e
Filip Talaga <talaga.filip@gmail.com>
2023-05-26 19:36:54 +0200

    Add left/right controls handle

--- public/controller.js
+++ public/controller.js
@@ -2,15 +2,15 @@ export const getController = () => {
   let controlsPressed = { space: false, left: false, right: false };
 
   const registerKey = ({ code }) => {
-    if (code === 'Space') {
-      controlsPressed.space = true;
-    }
+    if (code === 'Space') controlsPressed.space = true;
+    if (code === 'ArrowLeft') controlsPressed.left = true;
+    if (code === 'ArrowRight') controlsPressed.right = true;
   };
 
   const releaseKey = ({ code }) => {
-    if (code === 'Space') {
-      controlsPressed.space = false;
-    }
+    if (code === 'Space') controlsPressed.space = false;
+    if (code === 'ArrowLeft') controlsPressed.left = false;
+    if (code === 'ArrowRight') controlsPressed.right = false;
   };
 
   return { controlsPressed, registerKey, releaseKey };
