9083a1b85250c3735f0e048d05c7d08450e94016
Filip Talaga <talaga.filip@gmail.com>
2023-06-13 01:53:11 +0200

    Introduce webpack with typescript

---
+++ webpack.config.ts
@@ -0,0 +1,30 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+      favicon: 'public/favicon.ico',
+      hash: true,
+      minify: true,
+    }),
+  ],
+};
